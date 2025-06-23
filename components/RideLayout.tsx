7741ccb723ff25bc4c1e27a4cfe650b8f152eb41
Anmolzezx <anmolsingh7442@gmail.com>
2025-06-24 03:14:30 +0530

    implement find ride screen successfully and move to confirm ride

--- components/RideLayout.tsx
+++ components/RideLayout.tsx
@@ -1,19 +1,33 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { router } from "expo-router";
 import { icons } from "@/constants";
 import Map from "@/components/Map";
-import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
-import { useRef } from "react";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { useMemo, useRef } from "react";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const RideLayout = ({
   title,
   children,
+  snapPoints,
 }: {
   title: string;
   children: React.ReactNode;
+  snapPoints?: string[];
 }) => {
   const bottomSheetRef = useRef<BottomSheet>(null);
+  const memoizedSnapPoints = useMemo(
+    () => snapPoints || ["60%", "90%"],
+    [snapPoints],
+  );
   return (
     <GestureHandlerRootView>
       <View className="flex-1 bg-white">
@@ -34,10 +48,23 @@ const RideLayout = ({
           </View>
           <Map />
         </View>
-        <BottomSheet ref={bottomSheetRef} snapPoints={["40%", "85%"]} index={0}>
-          <BottomSheetScrollView style={{ flex: 1, padding: 20 }}>
-            {children}
-          </BottomSheetScrollView>
+        <BottomSheet
+          keyboardBehavior="interactive"
+          keyboardBlurBehavior="restore"
+          ref={bottomSheetRef}
+          snapPoints={memoizedSnapPoints}
+          index={0}
+        >
+          <BottomSheetView style={{ flex: 1, padding: 20 }}>
+            <KeyboardAwareScrollView
+              enableOnAndroid
+              keyboardShouldPersistTaps="handled"
+              contentContainerStyle={{ flexGrow: 1, padding: 20 }}
+              extraScrollHeight={200}
+            >
+              {children}
+            </KeyboardAwareScrollView>
+          </BottomSheetView>
         </BottomSheet>
       </View>
     </GestureHandlerRootView>
